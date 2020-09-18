4e6c4bd6e2c3e29cfc9005daec9c2b7054edd1d5
Appani Kaushik <58048398+kaushikappani@users.noreply.github.com>
2020-09-18 20:32:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-module.exports = {
-    iseven: (a) => {
-        if (a > 0) {
-            if (a % 2 == 0) {
-                return true
-            } else {
-                return false
-            }
+function iseven(a) {
+    if (a > 0) {
+        if (a % 2 == 0) {
+            return true
         } else {
-            return null
+            return false
         }
-    },
-    isodd: (a) => {
-        if (a > 0) {
-            if (a % 2 == 0) {
-                return false
-            } else {
-                return true
-            }
-        } else {
-            return null
-        }
-    },
-    pickodd: (array) => {
-        const odds = [];
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                continue;
-            } else {
-                odds.push(array[i])
-            }
-        }
-        return odds;
-    },
-    pickeven: (array) => {
-        const evens = [];
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                evens.push(array[i])
-            } else {
-                continue;
-            }
-        }
-        return evens;
+    } else {
+        return null
     }
+};
+
+function isodd(a) {
+    if (a > 0) {
+        if (a % 2 == 0) {
+            return false
+        } else {
+            return true
+        }
+    } else {
+        return null
+    }
+};
+
+function pickodd(array) {
+    const odds = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            continue;
+        } else {
+            odds.push(array[i])
+        }
+    }
+    return odds;
+};
+
+function pickeven(array) {
+    const evens = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            evens.push(array[i])
+        } else {
+            continue;
+        }
+    }
+    return evens;
+};
+
+module.exports = {
+    iseven,
+    isodd,
+    pickodd,
+    pickeven
 }
